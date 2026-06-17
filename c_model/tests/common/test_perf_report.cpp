a19ad67b3370627636608fd88aa3fb1e1113d7ea
yue831107 <mwzg1107@gmail.com>
2026-06-17 11:14:56 +0800

    test(perf): add PerfReport stdout summary + NOC_PERF JSON

---
+++ c_model/tests/common/test_perf_report.cpp
@@ -0,0 +1,26 @@
+#include "common/ni_perf_observer.hpp"
+#include "common/perf_report.hpp"
+#include "common/perf_common.hpp"
+#include <gtest/gtest.h>
+#include <sstream>
+#include <string>
+
+using namespace ni::cmodel::testing;
+
+TEST(PerfReport, SummaryLineContainsLabels) {
+    uint64_t now = 0;
+    PhaseController phase(now, PhaseConfig{});
+    NIPerfObserver ni(now, phase, [] { return std::size_t{0}; }, NIPerfConfig{"flowA"});
+    now = 1;
+    ni.on_issue(true, 1);
+    now = 4;
+    ni.on_complete(true, 1);
+
+    std::ostringstream os;
+    PerfReport rep;
+    rep.add_ni(&ni);
+    rep.write_summary(os);
+    const std::string out = os.str();
+    EXPECT_NE(out.find("[perf:flowA]"), std::string::npos);
+    EXPECT_NE(out.find("wr_lat"), std::string::npos);
+}
