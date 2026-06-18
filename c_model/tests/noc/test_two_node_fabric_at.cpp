d15b7f1f75fdfcede80f4f83c323557ef20a4913
yue831107 <mwzg1107@gmail.com>
2026-06-18 11:00:27 +0800

    test(noc): add TwoNodeFabric by-coordinate router accessors

---
+++ c_model/tests/noc/test_two_node_fabric_at.cpp
@@ -0,0 +1,14 @@
+#include "noc/two_node_fabric.hpp"
+#include <gtest/gtest.h>
+
+using ni::cmodel::noc::testing::TwoNodeFabric;
+
+TEST(TwoNodeFabricAt, CoordinateMapsToSameRouterAsNodeIndex) {
+    TwoNodeFabric ch(/*num_vc=*/1);
+    // 2x1 mesh: (0,0)=node 0, (1,0)=node 1. By-coordinate accessor must alias
+    // the by-node accessor (same Router object).
+    EXPECT_EQ(&ch.req_router_at(0, 0), &ch.req_router(0));
+    EXPECT_EQ(&ch.req_router_at(1, 0), &ch.req_router(1));
+    EXPECT_EQ(&ch.rsp_router_at(0, 0), &ch.rsp_router(0));
+    EXPECT_EQ(&ch.rsp_router_at(1, 0), &ch.rsp_router(1));
+}
